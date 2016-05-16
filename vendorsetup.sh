a5e74e6ced596b132d4cc0e2bd124be38f9b083e
Raphael Frajuca <olocogameplays552@gmail.com>
2016-05-16 15:21:50 -0300

    New vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,9 @@
-add_lunch_combo cm_logan2g-eng
-add_lunch_combo cm_logan2g-userdebug
+or i in eng userdebug user; do
+	add_lunch_combo cm_logan2g-${i}
+done
+
+function logan2g 
+{
+ mk_timer lunch cm_logan2g-userdebug
+ mk_timer mka bacon -j4
+}
